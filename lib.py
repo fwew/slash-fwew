fcbca4b75c457da3014639b0cae64b028922528b
Corey Scheideman <corscheid@gmail.com>
2023-01-20 11:07:58 -0600

    2.3.4: add ru lang default on new server

--- lib.py
+++ lib.py
@@ -5,7 +5,7 @@ import requests
 from space_containing import *
 from name_gen import *
 
-version = "2.3.3"
+version = "2.3.4"
 api_url = "http://localhost:10000/api"
 url_pattern = r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
 si_pattern = r"s(äp|eyk|äpeyk)?(iv|ol|er|am|ìm|ìy|ay|ilv|irv|imv|iyev|ìyev|alm|ìlm|ìly|aly|arm|ìrm|ìry|ary|ìsy|asy)?(eiy|äng|eng|uy|ats)?i"
@@ -21,7 +21,8 @@ def get_language(inter):
         860933619296108564: "en",
         1058520916612624536: "fr",
         1061696962304426025: "fr",
-        1065673594354548757: "ru"
+        1065673594354548757: "ru",
+        1063774395748847648: "ru"
     }
     if inter.guild is None:
         return "en"
